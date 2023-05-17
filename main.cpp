8a2dbf476870269467833675d012f83962f3c056
bullbesh <babulehaha@gmail.com>
2023-05-17 20:27:37 +0300

    Solutions modify

--- main.cpp
+++ main.cpp
@@ -6,33 +6,27 @@
 using namespace std;
  
 
-const int SIZE = 10; // размер поля
+const int SIZE = 8; // размер поля
 int queenCount = 0; // количество решений
 int board[SIZE][SIZE];
  
 int queenExistence(int a, int b) {
 // проверяет, нет ли уже установленных ферзей, по вертикали, диагоналям
 // 0 - ферзя нет, 1 - ферзь установлен
-    for (int i = 0; i < a; i++)
-    {
-        if (board[i][b])
-        {
+    for (int i = 0; i < a; i++) {
+        if (board[i][b]) {
             return 0;
         }
     }
  
-    for (int i = 1; (i <= a) && ((b - i) >= 0); i++)
-    {
-        if (board[a - i][b - i])
-        {
+    for (int i = 1; (i <= a) && ((b - i) >= 0); i++) {
+        if (board[a - i][b - i]) {
             return 0;
         }
     }
 
-    for (int i = 1; (i <= a) && ((b + i) < SIZE); i++)
-    {
-        if (board[a - i][b + i])
-        {
+    for (int i = 1; (i <= a) && ((b + i) < SIZE); i++) {
+        if (board[a - i][b + i]) {
             return 0;
         }
     }
@@ -48,7 +42,7 @@ void findSolutions(int a) {
     }
 
     for (int i = 0; i < SIZE; i++) {
-        // Здесь проверяем, что если поставим в board[a][i] ферзя (единицу),
+        // проверка на то, если поставить в board[a][i] ферзя (единицу),
         // то он будет единственным в этой строке, столбце и диагоналях
         if (queenExistence(a, i)) {
             board[a][i] = 1;
@@ -60,6 +54,6 @@ void findSolutions(int a) {
 
 int main() {
     findSolutions(0);
-    cout << queenCount << endl;
+    cout << "Solutions: " << queenCount << endl;
     return 0;
 }
